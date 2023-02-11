2c202fa4501798d92cfc4ffc9c15a52954ac22fd
Analise <analise.giobbi@gc.com>
2023-02-11 16:33:34 -0500

    fixing up index so that I can seed my data

--- models/Product.js
+++ models/Product.js
@@ -20,7 +20,7 @@ Product.init(
       allowNull: false,
     },
     price: {
-      type: DataTypes.DECIMAL(2,10),
+      type: DataTypes.DECIMAL,
       allowNull: false,
       validate: {
         isDecimal:true

a69514f1fe8f9103c41ab542ee05cec3e44d461a
Analise <analise.giobbi@gc.com>
2023-02-11 12:11:28 -0500

    updating product route

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -4,15 +4,29 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
-  // find all products
-  // be sure to include its associated Category and Tag data
+router.get('/', async (req, res) => {
+    try {
+      const productData = await Product.findAll();
+      res.status(200).json(productData)
+
+    } catch (err){
+      res.status(500).json(err)
+    }
 });
 
 // get one product
-router.get('/:id', (req, res) => {
-  // find a single product by its `id`
-  // be sure to include its associated Category and Tag data
+router.get('/:id', async (req, res) => {
+  try {
+    const productData = await Product.findByPk(req.params.id, {
+      include: [{ model: Category, through: Tag, as: 'product'}]
+    })
+    if (!productData){
+      res.status(404).json({ message: 'No Produces found with this id' })
+    }
+    res.status(200).json(productData)
+  }catch(err){
+    res.status(500).json(err)
+  }
 });
 
 // create new product
@@ -89,8 +103,19 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
-  // delete one product by its `id` value
+router.delete('/:id', async (req, res) => {
+  try {
+    const productData = await Product.destroy({
+      where: {
+        id: res.params.id
+      }
+    })
+    if (!productData){
+      res.status(404).json({ message: "there are no products associated with this id" })
+    }
+  }catch(err) {
+    res.status(500).json(err)
+  }
 });
 
 module.exports = router;
